6ecd0a5aeb95ad6ecc3a345b2a3902c1a3417608
tsw <trinityrushworth@gmail.com>
2024-03-30 22:25:25 +1300

    navbar

--- app.py
+++ app.py
@@ -66,3 +66,7 @@ def settings():
 @app.route("/login")
 def login():
     return(render_template("login.html"))
+
+@app.route("/register")
+def register():
+    return(render_template("register.html"))
